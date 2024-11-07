cc0c55fb5b63cbff5ca9ee2036eac6664630e117
Raja <raja@easymerchant.io>
2024-11-07 21:53:08 +0530

    simple default changes

--- checkout.js
+++ checkout.js
@@ -3,7 +3,7 @@ let elements;
      initialize();
 
      async function initialize() {
-          const { client_token, amount } = await fetch("./create.php", {
+          const { client_token, amount } = await fetch("/create.php", {
                method: "POST",
                headers: { "Content-Type": "application/json" },
           }).then((r) => r.json());
@@ -17,35 +17,29 @@ let elements;
                saveCard: true,
                saveAccount: true,
                submitButtonText: "Submit",
-               showReceipt: true, // true || false
+               showReceipt: false, // true || false
                showTotal: false, // true || false
                showSubmitButton: true, // true || false
                paymentMethods: ['card', "ach"], //'card', 'ach','crypto'
 
                // Optional
-
-
                //  fields: {
                //    billing: [
-
                //   { name: 'country', required: true, value: '' },
                //   { name: 'state', required: true, value: '' },
                //   { name: 'city', required: false,value: '' },
                //   { name: 'postal_code', required: true ,value: '' },
-
                // ],
                // additional: [
                // { name: 'name', required: true,value: '' },
                // { name: 'email_address', required: true ,value: ''},
                // { name: 'phone_number', required: false ,value: '' },
                // { name: 'description', required: true ,value: '' }
-
                // ]
                //   },
 
 
                // Optional
-
                apperanceSettings: {
                     bodyBackgroundColor: "#eeeff2",
                     containerBackgroundColor: "#ffffff",
@@ -57,41 +51,30 @@ let elements;
                     secondaryButtonBackgroundColor: "#ffffff",
                     secondaryButtonHoverColor: "#1757d9",
                     secondaryButtonFontColor: "#1757d9",
-
                     borderRadious: "8"
                }
-
           });
 
           // On Ready Event -  it will trigger once all elements render successfully
-
           elements.on('ready', (event) => {
                console.log("Ready", event)
           });
 
           // On Done Event - it will trigger once payment successfully done
           // or It will trigger once card or ach account token generated successfully
-
           elements.on('done', (event) => {
                console.log("Done", event)
-
           });
 
           // On Error Event - it will trigger once payment successfully done
-
           elements.on('error', (event) => {
                console.log("Error", event)
           });
 
-
-
           elements.on('process', (event) => {
                console.log("Process", event)
           });
-
      }
-
-
 
      // If you have your own button , you can call this function
 
