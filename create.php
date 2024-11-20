ea1b6bdd3228fbead90493332522a64e69530759
MariGanesh S <mariganesh@easymerchant.io>
2024-11-20 20:31:52 +0530

    Added content type json

--- create.php
+++ create.php
@@ -27,10 +27,10 @@ function curlCall($url='', $params='', $api_key='', $api_secret='')
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_POST,1);
 	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-Api-Key: " . $api_key,"X-Api-Secret: " . $api_secret, "User-Agent: JSSDK"));
+	curl_setopt($ch,CURLOPT_HTTPHEADER,array("X-Api-Key: " . $api_key,"X-Api-Secret: " . $api_secret, "User-Agent: JSSDK",'Content-Type: application/json'));
 	curl_setopt($ch, CURLOPT_POSTFIELDS,$params);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
+	
 	$results= curl_exec($ch);
 	return $results;
 }
